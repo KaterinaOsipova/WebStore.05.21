1959645a92fca604362264c150d9332a1e2e326f
Katerina Osipova <osipova3381@gmail.com>
2021-05-25 21:55:42 +0300

    Первое представление

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration Configuration;
+        public HomeController (IConfiguration Configuration) 
+        { 
+            this.Configuration = Configuration; 
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SecondAction()
+        {
+            return Content(Configuration ["Greetings"]);
+        }
+    }
+}
--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -14,21 +14,17 @@ namespace WebStore
 {
     public class Startup
     {
-
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        public Startup(IConfiguration Configuration)
+        {
+            this.Configuration = Configuration;
+        }
+ 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
