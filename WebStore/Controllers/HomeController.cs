d23830222f2be6df3beb4ecb23789f499fafdf6e
Katerina Osipova <osipova3381@gmail.com>
2021-05-25 23:26:40 +0300

    Сотрудники

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -4,16 +4,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 20 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 30 },
+            new Employee { Id = 2, LastName = "Федоров", FirstName = "Федор", Patronymic = "Федорович", Age = 40 },
+        };
+        
         private readonly IConfiguration Configuration;
         public HomeController (IConfiguration Configuration) 
         { 
             this.Configuration = Configuration; 
         }
+       
         public IActionResult Index()
         {
             return View();
@@ -22,6 +31,11 @@ namespace WebStore.Controllers
         public IActionResult SecondAction()
         {
             return Content(Configuration ["Greetings"]);
+        }
+
+        public IActionResult Employees()
+        {
+            return View(__Employees);
         }
     }
 }
