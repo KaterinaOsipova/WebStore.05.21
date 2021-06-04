b33cfb0bdb06a34e4d78186f0a5414abebed2108
Katerina Osipova <osipova3381@gmail.com>
2021-06-04 17:04:24 +0300

    Views of Blogs

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -50,5 +50,21 @@ namespace WebStore.Controllers
         }
 
         public IActionResult Blog() => View();
+
+        public IActionResult BlogSingle() => View();
+
+        public IActionResult Cart() => View();
+
+        public IActionResult Checkout() => View();
+
+        public IActionResult ContactUs() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult ProductDetails() => View();
+
+        public IActionResult Shop() => View();
+
+        public IActionResult Error404() => View();
     }
 }
